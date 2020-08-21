51e87cd6bcfdd686d69b45af22b45e2fe2089262
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-08-21 01:46:27 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -36,7 +36,20 @@ def initial_round
   return first_round
 end
 
-
+def hit?(card_total)
+  # code hit? here
+  prompt_user
+  user_input = get_user_input
+  if user_input == "h"
+    card_total += deal_card
+    elsif
+    user_input == "s"
+    card_total
+  else
+    invalid_command
+    hit?(card_total)
+end
+end
     
 
 def invalid_command

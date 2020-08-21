553c1031aacb0676dc0dc3d41532cac319b7e7e0
delightful-dispatch-9896 <maychen2580@hotmail.com>
2020-08-21 01:58:00 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -65,7 +65,7 @@ def runner
   # code runner here
   welcome
  card_total = initial_round
-  if card_total > 21
+  when card_total > 21
     card_total = hit?(card_total)
 display_card_total(card_total)
   end
